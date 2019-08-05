31734d1297816539412bbd47d82a1b8c0e155999
jaeho <ovsoce@gmail.com>
2019-08-05 09:51:04 +0900

    Change style

--- app/src/main/java/io/github/ovso/yearprogress/MainActivity.kt
+++ app/src/main/java/io/github/ovso/yearprogress/MainActivity.kt
@@ -1,8 +1,6 @@
 package io.github.ovso.yearprogress
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -10,12 +8,11 @@ import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.google.android.material.navigation.NavigationView
 import io.github.ovso.yearprogress.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.app_bar_main.toolbar
 import kotlinx.android.synthetic.main.content_main.bottomNavigationView
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
   private lateinit var viewModel: MainViewModel
 
@@ -26,18 +23,23 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
       DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     viewModel = provideViewModel()
     contentView.viewModel = viewModel
-    setSupportActionBar(toolbar)
+    setupActionBar()
+    setupDrawer()
+    setupBottonNavView()
+  }
 
+  private fun setupActionBar() {
+    setSupportActionBar(toolbar)
+    title = getString(R.string.main_title)
+  }
+
+  private fun setupDrawer() {
     val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-    val navView: NavigationView = findViewById(R.id.nav_view)
     val toggle = ActionBarDrawerToggle(
       this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
     )
     drawerLayout.addDrawerListener(toggle)
     toggle.syncState()
-    navView.setNavigationItemSelectedListener(this)
-
-    setupBottonNavView()
   }
 
   private fun provideViewModel() = ViewModelProviders.of(this).get(MainViewModel::class.java)
@@ -77,46 +79,4 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
   }
 
-  override fun onCreateOptionsMenu(menu: Menu): Boolean {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    menuInflater.inflate(R.menu.main, menu)
-    return true
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    return when (item.itemId) {
-      R.id.action_settings -> true
-      else -> super.onOptionsItemSelected(item)
-    }
-  }
-
-  override fun onNavigationItemSelected(item: MenuItem): Boolean {
-    // Handle navigation view item clicks here.
-    when (item.itemId) {
-      R.id.nav_home -> {
-        // Handle the camera action
-      }
-      R.id.nav_gallery -> {
-
-      }
-      R.id.nav_slideshow -> {
-
-      }
-      R.id.nav_tools -> {
-
-      }
-      R.id.nav_share -> {
-
-      }
-      R.id.nav_send -> {
-
-      }
-    }
-    val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-    drawerLayout.closeDrawer(GravityCompat.START)
-    return true
-  }
 }
