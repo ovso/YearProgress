762b1bdefd8e6a8dc1b940e657e7148213debda7
jaeho <ovsoce@gmail.com>
2019-08-07 13:04:50 +0900

    Custom nav menu

--- app/src/main/java/io/github/ovso/yearprogress/MainActivity.kt
+++ app/src/main/java/io/github/ovso/yearprogress/MainActivity.kt
@@ -1,6 +1,10 @@
 package io.github.ovso.yearprogress
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -8,9 +12,11 @@ import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import de.psdev.licensesdialog.LicensesDialog
 import io.github.ovso.yearprogress.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.app_bar_main.toolbar
 import kotlinx.android.synthetic.main.content_main.bottomNavigationView
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -79,4 +85,44 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    item?.let {
+      when (item.itemId) {
+        R.id.nav_opensource -> showLicenseDialog()
+        R.id.nav_review -> navigateToReview()
+        R.id.nav_share -> navigateToShare()
+      }
+    }
+    return super.onOptionsItemSelected(item)
+  }
+
+  private fun navigateToShare() {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+      type = "text/plain"
+      addCategory(Intent.CATEGORY_DEFAULT)
+      putExtra(Intent.EXTRA_TITLE, "Share")
+      putExtra(Intent.EXTRA_TEXT, "market://details?value=$packageName")
+    }
+    startActivity(Intent.createChooser(intent, "App share"))
+  }
+
+  private fun navigateToReview() {
+    val intent = Intent(Intent.ACTION_VIEW).apply {
+      val uriString = "https://play.google.com/store/apps/details?id=$packageName"
+      data = Uri.parse(uriString)
+      setPackage("com.android.vending")
+    }
+    try {
+      startActivity(intent)
+    } catch (e: ActivityNotFoundException) {
+      Timber.e(e)
+    }
+  }
+
+  private fun showLicenseDialog() {
+    LicensesDialog.Builder(this)
+      .setNotices(R.raw.notices)
+      .build()
+      .show()
+  }
 }
