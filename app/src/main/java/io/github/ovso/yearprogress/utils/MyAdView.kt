eb19939af17ea3f94cf090932c0df49385ce2cee
jaeho <ovsoce@gmail.com>
2019-09-01 16:50:16 +0900

    Added MyAdView

---
+++ app/src/main/java/io/github/ovso/yearprogress/utils/MyAdView.kt
@@ -0,0 +1,27 @@
+package io.github.ovso.yearprogress.utils
+
+import android.content.Context
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.InterstitialAd
+
+object MyAdView {
+  fun getAdmobBannerView(context: Context): AdView {
+    val adView = AdView(context)
+    adView.adSize = AdSize.FULL_BANNER
+    adView.adUnitId = Ads.ADMOB_BANNER_UNIT_ID.value
+    val adRequest = AdRequest.Builder()
+      .build()
+    adView.loadAd(adRequest)
+    return adView
+  }
+
+  fun getAdmobInterstitialAd(context: Context): InterstitialAd {
+    val interstitialAd = InterstitialAd(context)
+    interstitialAd.adUnitId = Ads.ADMOB_INTERSTITIAL_ID.value
+    val adRequestBuilder = AdRequest.Builder()
+    interstitialAd.loadAd(adRequestBuilder.build())
+    return interstitialAd
+  }
+}
