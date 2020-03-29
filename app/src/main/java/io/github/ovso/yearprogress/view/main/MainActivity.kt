24f3315f90734d4dad141d6c60759368e1da7706
jaeho <ovsoce@gmail.com>
2020-03-30 07:49:32 +0900

    Code reformat

--- app/src/main/java/io/github/ovso/yearprogress/view/main/MainActivity.kt
+++ app/src/main/java/io/github/ovso/yearprogress/view/main/MainActivity.kt
@@ -29,124 +29,124 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var viewModel: MainViewModel
+    private lateinit var viewModel: MainViewModel
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    val contentView =
-      DataBindingUtil.setContentView<ActivityMainBinding>(
-        this,
-        layout.activity_main
-      )
-    viewModel = provideViewModel()
-    contentView.viewModel = viewModel
-    setupActionBar()
-    setupDrawer()
-    setupBottomNavView()
-    setupNavigationView()
-    setupAds()
-  }
-
-  private fun setupAds() {
-    fl_ads_container.addView(MyAdView.getAdmobBannerView(this))
-  }
-
-  private fun setupNavigationView() {
-    nav_view.setNavigationItemSelectedListener {
-      drawer_layout.closeDrawer(GravityCompat.START)
-      when (it.itemId) {
-        id.nav_opensource -> showLicenseDialog()
-        id.nav_review -> navigateToReview()
-        id.nav_share -> navigateToShare()
-      }
-
-      true
-    }
-  }
-
-  private fun setupActionBar() {
-    setSupportActionBar(toolbar)
-    title = getString(string.main_title)
-  }
-
-  private fun setupDrawer() {
-    val toggle = ActionBarDrawerToggle(
-      this,
-      drawer_layout,
-      toolbar,
-      string.navigation_drawer_open,
-      string.navigation_drawer_close
-    )
-    drawer_layout.addDrawerListener(toggle)
-    toggle.syncState()
-  }
-
-  private fun provideViewModel() = ViewModelProviders.of(this).get(MainViewModel::class.java)
-
-  private fun setupBottomNavView() {
-    bottomNavigationView.setOnNavigationItemSelectedListener {
-      when (it.itemId) {
-        id.bottom_nav_year -> viewModel.navSelectLiveData.value = BottomNav.YEAR.index
-        id.bottom_nav_month -> viewModel.navSelectLiveData.value = BottomNav.MONTH.index
-        id.bottom_nav_day -> viewModel.navSelectLiveData.value = BottomNav.DAY.index
-      }
-      true
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val contentView =
+                DataBindingUtil.setContentView<ActivityMainBinding>(
+                        this,
+                        layout.activity_main
+                )
+        viewModel = provideViewModel()
+        contentView.viewModel = viewModel
+        setupActionBar()
+        setupDrawer()
+        setupBottomNavView()
+        setupNavigationView()
+        setupAds()
     }
 
-    viewModel.navSelectLiveData.observe(this, Observer {
-      replaceFragment(it)
-    })
-    val index = intent.getIntExtra(EXTRA_NAME_INDEX, 0)
-    viewModel.navSelectLiveData.postValue(index)
-    val menuItem = bottomNavigationView.menu.get(index)
-    menuItem.setChecked(true)
-  }
-
-  private fun replaceFragment(position: Int) {
-    supportFragmentManager.beginTransaction()
-      .replace(
-        id.framelayout_main_replace_container,
-        ProgressFragment.newInstance(position),
-        ProgressFragment::class.java.simpleName
-      ).commitNowAllowingStateLoss()
-  }
-
-  override fun onBackPressed() {
-    if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-      drawer_layout.closeDrawer(GravityCompat.START)
-    } else {
-      super.onBackPressed()
-    }
-  }
-
-  private fun navigateToShare() {
-    val intent = Intent(Intent.ACTION_SEND).apply {
-      type = "text/plain"
-      addCategory(Intent.CATEGORY_DEFAULT)
-      putExtra(Intent.EXTRA_TITLE, "Share")
-      putExtra(Intent.EXTRA_TEXT, "market://details?value=$packageName")
-    }
-    startActivity(Intent.createChooser(intent, "App share"))
-  }
-
-  private fun navigateToReview() {
-    val intent = Intent(Intent.ACTION_VIEW).apply {
-      val uriString = "https://play.google.com/store/apps/details?id=$packageName"
-      data = Uri.parse(uriString)
-      setPackage("com.android.vending")
-    }
-    try {
-      startActivity(intent)
-    } catch (e: ActivityNotFoundException) {
-      Timber.e(e)
+    private fun setupAds() {
+        fl_ads_container.addView(MyAdView.getAdmobBannerView(this))
     }
 
-  }
+    private fun setupNavigationView() {
+        nav_view.setNavigationItemSelectedListener {
+            drawer_layout.closeDrawer(GravityCompat.START)
+            when (it.itemId) {
+                id.nav_opensource -> showLicenseDialog()
+                id.nav_review -> navigateToReview()
+                id.nav_share -> navigateToShare()
+            }
 
-  private fun showLicenseDialog() {
-    LicensesDialog.Builder(this)
-      .setNotices(raw.notices)
-      .build()
-      .show()
-  }
+            true
+        }
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar)
+        title = getString(string.main_title)
+    }
+
+    private fun setupDrawer() {
+        val toggle = ActionBarDrawerToggle(
+                this,
+                drawer_layout,
+                toolbar,
+                string.navigation_drawer_open,
+                string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    private fun provideViewModel() = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+    private fun setupBottomNavView() {
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                id.bottom_nav_year -> viewModel.navSelectLiveData.value = BottomNav.YEAR.index
+                id.bottom_nav_month -> viewModel.navSelectLiveData.value = BottomNav.MONTH.index
+                id.bottom_nav_day -> viewModel.navSelectLiveData.value = BottomNav.DAY.index
+            }
+            true
+        }
+
+        viewModel.navSelectLiveData.observe(this, Observer {
+            replaceFragment(it)
+        })
+        val index = intent.getIntExtra(EXTRA_NAME_INDEX, 0)
+        viewModel.navSelectLiveData.postValue(index)
+        val menuItem = bottomNavigationView.menu.get(index)
+        menuItem.setChecked(true)
+    }
+
+    private fun replaceFragment(position: Int) {
+        supportFragmentManager.beginTransaction()
+                .replace(
+                        id.framelayout_main_replace_container,
+                        ProgressFragment.newInstance(position),
+                        ProgressFragment::class.java.simpleName
+                ).commitNowAllowingStateLoss()
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun navigateToShare() {
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            addCategory(Intent.CATEGORY_DEFAULT)
+            putExtra(Intent.EXTRA_TITLE, "Share")
+            putExtra(Intent.EXTRA_TEXT, "market://details?value=$packageName")
+        }
+        startActivity(Intent.createChooser(intent, "App share"))
+    }
+
+    private fun navigateToReview() {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            val uriString = "https://play.google.com/store/apps/details?id=$packageName"
+            data = Uri.parse(uriString)
+            setPackage("com.android.vending")
+        }
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Timber.e(e)
+        }
+
+    }
+
+    private fun showLicenseDialog() {
+        LicensesDialog.Builder(this)
+                .setNotices(raw.notices)
+                .build()
+                .show()
+    }
 }
