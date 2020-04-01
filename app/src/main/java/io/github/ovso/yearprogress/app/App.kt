f599eb12f0686599f13e60ed8fc6e8a915958bde
jaeho <ovsoce@gmail.com>
2020-04-01 23:35:58 +0900

    Optimize imports

--- app/src/main/java/io/github/ovso/yearprogress/app/App.kt
+++ app/src/main/java/io/github/ovso/yearprogress/app/App.kt
@@ -3,8 +3,6 @@ package io.github.ovso.yearprogress.app
 import android.app.Application
 import com.google.android.gms.ads.MobileAds
 import com.jakewharton.threetenabp.AndroidThreeTen
-import io.github.ovso.yearprogress.Ads
-import io.github.ovso.yearprogress.InitializeLibs
 
 class App : Application() {
 
